e6c0446c651a82570033709521ed67358c454c1d
Petio Petrov <petioptrv@icloud.com>
2025-08-17 18:16:26 +0700

    (fix) prestart.sh

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -4,10 +4,9 @@ set -e
 set -x
 
 # Let the DB start
-python src/pre_start.py
+PYTHONPATH=./:$PYTHONPATH python src/pre_start.py
 
 # Run migrations
 alembic upgrade head
 
-# Create initial data in DB
 PYTHONPATH=./:$PYTHONPATH python src/initial_data.py
