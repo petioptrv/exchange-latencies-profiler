d734f6e474d6c08ce300964cb61f6664a3106dd8
Petio Petrov <petioptrv@icloud.com>
2025-09-29 17:35:38 +0700

    (refactor) Changes to logging

--- scripts/ec2_run.sh
+++ scripts/ec2_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd ~/exchange-latencies-profiler || retrn
+
 source venv/bin/activate
+PYTHONPATH=./ python src/profiler_app.py
--- src/logging_utils.py
+++ src/logging_utils.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 from src.config import settings
 from src.constants import PROJECT_ROOT
@@ -10,11 +11,19 @@ def setup_logging():
 
     log_file = log_dir / "app.log"
 
+    rotating_handler = TimedRotatingFileHandler(
+        filename=log_file,
+        when="D",
+        interval=1,
+        backupCount=2,
+        encoding="utf-8"
+    )
+
     logging.basicConfig(
         level=getattr(logging, settings.LOGGING_LEVEL),
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[
-            logging.FileHandler(log_file),
+            rotating_handler,
             logging.StreamHandler()
         ]
     )
